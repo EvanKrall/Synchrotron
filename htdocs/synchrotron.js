ddcef931a3edbe8bebd39fd0859caf7185d1dfea
Evan Krall <krall@yelp.com>
2011-07-29 13:21:06 -0700

    Hey i can play, pause, seek, etc.

--- htdocs/synchrotron.js
+++ htdocs/synchrotron.js
@@ -1,15 +1,157 @@
 $(document).ready(function() {
-	showStatus("Connecting to Synchrotron");
+	var STATE_NOT_LOADING = 'not loading';
+	var STATE_LOADING = 'loading';
+	var STATE_LOADED_AND_PAUSED = 'paused';
+	var STATE_PLAYING = 'playing';
+	var STATE_ENDED = 'ended';
+	var STATE_SEEKING = 'seeking';
 
-	DNode.connect(function (remote) {
-		showStatus("Connected. Registering with Synchrotron");
-		remote.register(window.location.hash, new Client(), function(id) {
-			showStatus("Registered with id " + id);
-		})
-	});
+	var client_id;
+
+	showStatus("Loading video metadata.");
+
+	// Start loading the video. Apparently .load() doesn't work.
+	video = $('#video')[0];
+	video.load(); video.play(); video.pause();
+	$('#video').one('loadedmetadata', function() {
+		showStatus("Connecting to Synchrotron");
+		DNode.connect(function (server) {
+			showStatus("Connected. Registering with Synchrotron");
+			server.register(window.location.hash, new Client(), function(id) {
+				showStatus("Registered with id " + id);
+				client_id = id;
+				// $('#video').bind('playing pause	stalled canplaythrough seeking seeked', function(event) {
+				// 	 server.reportEvent(id, event.type, getStatus());
+				// });
+			})
+		});
+	})
 
 	function Client() {
-		this.log = log;
+		// TODO make these into getters that calculate this shit.
+		var goalState = STATE_NOT_LOADING;
+		var goalTime = 0;
+
+		this.setGoal = function(state, time) {
+			showStatus("Going for goal " + state + ", " + time);
+			goalState = state;
+			goalTime = time;
+			if (tryForGoal[goalState]) {
+				tryForGoal[goalState](time);
+			}
+		}
+	}
+
+	var PLAY_THEN_PAUSE_THRESHOLD = 5;
+	var CLOSE_ENOUGH_THRESHOLD = 2;
+	var tryForGoal = {};
+	tryForGoal[STATE_LOADED_AND_PAUSED] = function(time) {
+		video = $('#video')[0];
+		$(video).unbind('.aoeu');
+		if (video.currentTime < time && time-video.currentTime < PLAY_THEN_PAUSE_THRESHOLD && !video.paused) {
+			// Wait until we catch up to the time, then pause.
+			log("Goal time is only " + (time-video.currentTime) + " seconds away. Waiting to catch up, then pausing");
+			$(video).bind('timeupdate.aoeu', function() {
+				if (video.currentTime > time) {
+					$(video).unbind('timeupdate.aoeu');
+					video.pause();
+					reportWhenReady();
+				}
+			});
+		} else if (Math.abs(video.currentTime - time) < CLOSE_ENOUGH_THRESHOLD) {
+			// Close enough. Just pause here.
+			video.pause();
+			reportWhenReady();
+		} else {
+			// We'll need to seek.
+			video.currentTime = time;
+			video.pause();
+			$(video).one('seeked.aoeu', function() {
+				reportWhenReady();
+			});
+		}
+	}
+
+	tryForGoal[STATE_PLAYING] = function(time) {
+		video = $('#video')[0];
+		if (video.currentTime < time && time-video.currentTime < PLAY_THEN_PAUSE_THRESHOLD && !video.paused) {
+			// Wait until we catch up to the time, then report.
+			log("Goal time is only " + (time-video.currentTime) + " seconds away. Waiting to catch up.");
+			$(video).bind('timeupdate.aoeu', function() {
+				if (video.currentTime > time) {
+					$(video).unbind('timeupdate.aoeu');
+					reportWhenPlaying();
+				}
+			});
+		} else if (Math.abs(video.currentTime - time) < CLOSE_ENOUGH_THRESHOLD) {
+			// Close enough, just play here.
+			if (video.readyState < video.HAVE_FUTURE_DATA) {
+				log("Not good. I can't play yet. You shouldn't be asking me to.");
+			}
+			video.play();
+			reportWhenPlaying();
+		} else {
+			// We're far away. We'll need to seek.
+			video.currentTime = time;
+			video.play();
+			$(video).one('seeked.aoeu', function() {
+				reportWhenPlaying();
+			});
+		}
+	}
+
+	function reportWhenReady() {
+		// When we have enough data to play through to the end, call reportState.
+		video=$('#video')[0];
+		if (video.readyState < video.HAVE_ENOUGH_DATA) {
+			$(video).one('canplaythrough.aoeu', function() {
+				reportState();
+			});
+		} else {
+			reportState();
+		}
+	}
+
+	function reportWhenPlaying() {
+		// When we start playing, reportState.
+		video=$('#video')[0];
+		if (!isPlaying(video)) {
+			$(video).one('playing.aoeu', function() {
+				reportState();
+			});
+		} else {
+			reportState();
+		}
+	}
+
+	function reportState() {
+
+	}
+
+	function calculateState() {
+		video = $('#video')[0];
+		if (video.ended) {
+			return STATE_ENDED;
+		}
+		if (video.readyState < video.HAVE_FUTURE_DATA) {
+			if (networkState === video.NETWORK_LOADING) {
+				return STATE_LOADING;
+			} else {
+				return STATE_NOT_LOADING;
+			}
+		} else {
+			if (video.paused) {
+				return STATE_LOADED_AND_PAUSED;
+			} else if (video.seeking) {
+				return STATE_SEEKING;
+			} else {
+				return STATE_PLAYING;
+			}
+		}
+	}
+
+	function isPlaying(video) {
+		return !(video.paused || video.ended || video.seeking || video.readyState < video.HAVE_FUTURE_DATA);
 	}
 
 	function showStatus(status) {
@@ -21,5 +163,3 @@ $(document).ready(function() {
 		$('#log').append($('<div class="logmessage"></div>').text(message))
 	}
 });
-
-
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ console.log('http://localhost:8080/');
 var dnode = require('dnode');
 
 var clients = {};
+var video_id_by_client_id = {};
 var videos = {};
 
 function makeId() {
@@ -18,28 +19,64 @@ function makeId() {
 	});
 }
 
+var STATE_NOT_LOADING = 'not loading';
+var STATE_LOADING = 'loading';
+var STATE_PAUSED = 'paused';
+var STATE_PLAYING = 'playing';
 
-function Video(client) {
-	this.clients = [client];
+var THRESHOLD = 5; // The maximum timedelta in seconds that we can tolerate between where we are and where the server is.
+
+function Video(firstClient) {
+	var clients = firstClient? [firstClient] : [];
+	// List of 2-tuples containing the state and the goal time.
+	var plan = [[STATE_PLAYING, 0]];
+	var planNum = 0;
+
+	this.addClient = function(client) {
+		clients.append(client);
+	};
+
+	this.setPlan = function(_plan) {
+		planNum++;
+		plan = _plan;
+		setGoal(plan[0].state, plan[0].time);
+	};
+
+	function setGoal(goal, time) {
+		myPlanNum = planNum;
+		for (var i=0; i<clients.length; i++) {
+			clients[i].setGoal(goal, time);
+		}
+	}
 }
 
-var server = dnode({
-	register : function(video_id, client, callback) {
-		id = makeId();
-		client.id = id;
-		clients[id] = client;
-		if (! video_id) {
-			video_id = makeId();
+var server = dnode(function(connection) {
+	return {
+		register : function(video_id, client, callback) {
+			id = makeId();
+			client.id = id;
+			clients[id] = client;
+			if (! video_id) {
+				video_id = makeId();
+			}
 			videos[video_id] = new Video(client);
-		}
-		callback(id, video_id);
-	},
-	ping : function(id, status) {
-		console.log("Ping", name, status);
-	},
-	reportEvent : function(id, event) {
-		console.log("Event", name, event);
-	}
+			callback(id, video_id);
+			setTimeout(function() {client.setGoal('paused', 20);}, 1000);
+			setTimeout(function() {client.setGoal('playing', 20);}, 5000);
+			setTimeout(function() {client.setGoal('paused', 25);}, 9000);
+			setTimeout(function() {client.setGoal('playing', 25);}, 12000);
+			setTimeout(function() {client.setGoal('playing', 10);}, 15000);
+		},
+		stateChange : function(id, state) {
+			console.log("stateChange", name, state);
+		},
+		// reportEvent : function(id, event_type, status) {
+		// 	console.log("reportEvent", id, event_type, status);
+		// },
+		ping : function(callback) {
+			callback();
+		},
+	};
 });
 
 server.listen(app);
